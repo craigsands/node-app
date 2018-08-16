7ddf5a10812972a826ac16f938d2011e9bac3de8
Craig Sands <craig.sands@lexisnexisrisk.com>
2018-08-16 17:09:13 -0400

    Initial commit

--- config/lc.tf
+++ config/lc.tf
@@ -2,6 +2,10 @@ resource "aws_launch_configuration" "node_app" {
   image_id      = "${data.aws_ami.node_app.id}"
   instance_type = "t2.small"
   security_groups = ["${aws_security_group.node_app.id}"]
+  user_data = <<EOF
+cd /usr/local/node-app
+pm2 start ./bin/www --name='app'
+EOF
 
   lifecycle {
     create_before_destroy = true
